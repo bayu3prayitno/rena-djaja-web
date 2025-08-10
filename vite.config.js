684f375d712180baff806c3e653f2bb74c8d558c
bayu3prayitno <treeprayitno12@gmail.com>
2025-08-10 07:14:59 +0700

    edit vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base : 'https://github.com/bayu3prayitno/rena-djaja.git',
+  base: '/rena-djaja/',
   plugins: [react(), tailwindcss()],
 })
